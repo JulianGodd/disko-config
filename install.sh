6dba82e2bdaea6fd133c53e4452d5287c193f92b
JulianGodd <juliandgoddard@gmail.com>
2025-02-10 00:35:54 -0700

    Improved install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,24 @@
 
 curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/disko.nix -o /tmp/disko.nix
 
+echo -n "Encryption Password: "
 read pass
-
-echo this is \"$pass\"
+echo
 
 echo -n "$pass" > /tmp/secret.key
 
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/nvme0n1"'
 
 sudo nixos-generate-config --no-filesystems --root /mnt
+
+# Curl configuration and flake 
+
+curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/nixos/configuration.nix -o /mnt/etc/nixos/configuration.nix
+curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/nixos/flake.nix -o /mnt/etc/nixos/flake.nix
+
+mv /tmp/disko.nix /mnt/etc/nixos/flake.nix
+
+cd /mnt/etc/nixos
+
+echo Change the username in configuration.nix and the drive in flake.nix, then run:
+echo "    nixos-install --root /mnt --flake /mnt/etc/nixos#default"
---
+++ nixos/configuration.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, lib, ... }:
+{
+    imports = [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+    ];
+
+    boot.loader = { # Bootloader
+        #systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+    };
+
+    boot.initrd = { # TPM
+        systemd.enable = true;
+        availableKernelModules = [ "tpm_crb"];
+    };
+
+    #boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+
+    #environment.etc."crypttab.initramfs".text = ''
+    #    root UUID="..." none tpm2-device=auto
+    #'';
+
+    users.users."someuser" = {
+        isNormalUser = true;
+        initialPassword = "1";
+        extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
+    nix = {
+        settings.experimental-features = [ "nix-command" "flakes" ];
+
+        gc = { # Cleanup old system generations after a month
+            options = "--delete-older-than 31d";
+            persistent = true;
+            dates = "monthly";
+            automatic = true;
+        };
+    };
+
+    time.timeZone = "America/Denver";
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    services.xserver.enable = true;
+
+    system.stateVersion = "24.05"; # Don't change this
+}
