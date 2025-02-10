89764560631bfae2a58a2a45dd8a7037502632e1
JulianGodd <juliandgoddard@gmail.com>
2025-02-10 01:04:45 -0700

    Removed grub partition, simplified disko

--- disko.nix
+++ disko.nix
@@ -9,14 +9,9 @@
             content = {
                 type = "gpt";
                 partitions = {
-                    boot = {
-                        name = "boot";
-                        size = "1M";
-                        type = "EF02";
-                    };
                     esp = {
                         name = "ESP";
-                        size = "500M";
+                        size = "512M";
                         type = "EF00";
                         content = {
                             type = "filesystem";
@@ -25,23 +20,8 @@
                             mountOptions = [ "umask=0077" ];
                         };
                     };
-                    root = {
-                        name = "root";
+                    luks = {
                         size = "100%";
-                        content = {
-                            type = "lvm_pv";
-                            vg = "root_vg";
-                        };
-                    };
-                };
-            };
-        };
-        lvm_vg = {
-            root_vg = {
-                type = "lvm_vg";
-                lvs = {
-                    root = {
-                        size = "100%FREE";
                         content = {
                             type = "luks";
                             name = "crypted";
