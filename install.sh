ca3da2e970348cd519e624984198e350d07920fd
JulianGodd <juliandgoddard@gmail.com>
2025-02-10 00:45:52 -0700

    install tweaks

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ sudo nixos-generate-config --no-filesystems --root /mnt
 sudo curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/nixos/configuration.nix -o /mnt/etc/nixos/configuration.nix --clobber
 sudo curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/nixos/flake.nix -o /mnt/etc/nixos/flake.nix --clobber
 
-mv /tmp/disko.nix /mnt/etc/nixos/flake.nix
+mv /tmp/disko.nix /mnt/etc/nixos/disko.nix
 
 cd /mnt/etc/nixos
 
