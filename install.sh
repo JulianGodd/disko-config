71d074ae42801175c9d83d9daf5b7a1628e3e952
JulianGodd <juliandgoddard@gmail.com>
2025-02-10 00:39:43 -0700

    install tweaks

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/disko.nix -o /tmp/disko.nix
-
 echo -n "Encryption Password: "
 read pass
 echo
 
 echo -n "$pass" > /tmp/secret.key
+
+curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/disko.nix -o /tmp/disko.nix
 
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/nvme0n1"'
 
@@ -14,8 +14,8 @@ sudo nixos-generate-config --no-filesystems --root /mnt
 
 # Curl configuration and flake 
 
-curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/nixos/configuration.nix -o /mnt/etc/nixos/configuration.nix
-curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/nixos/flake.nix -o /mnt/etc/nixos/flake.nix
+sudo curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/nixos/configuration.nix -o /mnt/etc/nixos/configuration.nix
+sudo curl https://raw.githubusercontent.com/JulianGodd/disko-config/refs/heads/main/nixos/flake.nix -o /mnt/etc/nixos/flake.nix
 
 mv /tmp/disko.nix /mnt/etc/nixos/flake.nix
 
