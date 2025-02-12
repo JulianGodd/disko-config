e5cfbd94cd11ee1b471048b9e6382c650077b83d
JulianGodd <juliandgoddard@gmail.com>
2025-02-12 00:36:30 -0700

    disko changes

--- disko.nix
+++ disko.nix
@@ -1,67 +1,75 @@
 {
     device ? throw "Set this to your disk device, e.g. /dev/sda",
-    ...
+           ...
 }: {
     disko.devices = {
-        disk.main = {
-            inherit device;
-            content = {
-                type = "gpt";
-                partitions = {
-                    esp = {
-                        name = "ESP";
-                        size = "512M";
-                        type = "EF00";
-                        content = {
-                            type = "filesystem";
-                            format = "vfat";
-                            mountpoint = "/boot";
-                            mountOptions = [ "umask=0077" ];
-                        };
-                    };
-                    crypt-main = {
-                        size = "100%";
-                        content = {
-                            type = "luks";
-                            name = "crypt-main";
+        disk = {
+            main = {
+                type = "disk";
+                device = "/dev/vdb";
+                content = {
+                    type = "gpt";
+                    partitions = {
+                        ESP = {
+                            size = "500M";
+                            type = "EF00";
                             content = {
-                                type = "lvm_pv";
-                                vg = "main";
+                                type = "filesystem";
+                                format = "vfat";
+                                mountpoint = "/boot";
+                                mountOptions = [ "umask=0077" ];
                             };
-                            settings = {
-                                bypassWorkqueues = true;
-                                allowDiscards = true;
-                                crypttabExtraOpts = [ "tpm2-device=auto" "tpm2-measure-pcr=yes" ];
+                        };
+                        luks = {
+                            size = "100%";
+                            content = {
+                                type = "luks";
+                                name = "crypt-main";
+                                extraOpenArgs = [ ];
+                                additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                                content = {
+                                    type = "lvm_pv";
+                                    vg = "pool";
+                                };
+                                settings = {
+                                    bypassWorkqueues = true;
+                                    keyFile = "/tmp/secret.key";
+                                    allowDiscards = true;
+                                    crypttabExtraOpts = [ "tpm2-device=auto" "tpm2-measure-pcr=yes" ];
+                                };
                             };
                         };
                     };
                 };
             };
         };
-
-        lvm_vg.main.lvs = {
-            nix.size = "100G";
-            persist.size = "100G";
-            swap = {
-                size = "30G";
-                lvm_type = "thinlv";
-                pool = "thin-main";
-                content = {
-                    type = "swap";
-                    randomEncryption = true;
-                };
-            };
-            crypt-home-someuser = {
-                size = "100G";
-                lvm_type = "thinlv";
-                pool = "thin-main";
-                content = {
-                    type = "luks";
-                    name = "home-someuser";
-                    content = {
-                        type = "filesystem";
-                        format = "btrfs";
-                        mountOptions = [ "noatime" ];
+        lvm_vg = {
+            pool = {
+                type = "lvm_vg";
+                lvs = {
+                    thin-main = {
+                        size = "95%FREE";
+                        lvm_type = "thin-pool";
+                    };
+                    nix = {
+                        lvm_type = "thinlv";
+                        pool = "thin-main";
+                        content = {
+                            type = "filesystem";
+                            format = "btrfs";
+                            mountpoint = "/nix";
+                            mountOptions = [ "noatime" ];
+                        };
+                    };
+                    persist = {
+                        lvm_type = "thinlv";
+                        pool = "thin-main";
+                        content = {
+                            type = "filesystem";
+                            format = "btrfs";
+                            mountpoint = "/mnt/persist";
+                            mountOptions = [ "noatime" ];
+                        };
                     };
                 };
             };
