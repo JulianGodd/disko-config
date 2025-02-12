d82a3b7a5e9dabc87ed4b9d4495ca8564d887f88
JulianGodd <juliandgoddard@gmail.com>
2025-02-12 00:14:03 -0700

    Attempting other btrfs disko config

--- disko.nix
+++ disko.nix
@@ -1,57 +1,63 @@
 {
     device ? throw "Set this to your disk device, e.g. /dev/sda",
-           ...
 }: {
     disko.devices = {
         disk.main = {
             inherit device;
-            type = "disk";
-            content = {
-                type = "gpt";
-                partitions = {
-                    esp = {
-                        name = "ESP";
-                        size = "512M";
-                        type = "EF00";
+
+            content.partitions = {
+                esp = {
+                    name = "ESP";
+                    size = "512M";
+                    type = "EF00";
+                    content = {
+                        type = "filesystem";
+                        format = "vfat";
+                        mountpoint = "/boot";
+                        mountOptions = [ "umask=0077" ];
+                    };
+                };
+                crypt-main = {
+                    size = "100%";
+                    content = {
+                        type = "luks";
+                        name = "crypt-main";
                         content = {
-                            type = "filesystem";
-                            format = "vfat";
-                            mountpoint = "/boot";
-                            mountOptions = [ "umask=0077" ];
+                            type = "lvm_pv";
+                            vg = "main";
+                        };
+                        settings = {
+                            bypassWorkqueues = true;
+                            allowDiscards = true;
+                            crypttabExtraOpts = [ "tpm2-device=auto" "tpm2-measure-pcr=yes" ];
                         };
                     };
-                    luks = {
-                        size = "100%";
-                        content = {
-                            type = "luks";
-                            name = "crypted";
-                            settings = {
-                                allowDiscards = true;
-                                keyFile = "/tmp/secret.key";
-                                crypttabExtraOpts = [ "tpm2-device=auto" "tpm2-measure-pcr=yes" ];
-                            };
-                            content = {
-                                type = "btrfs";
-                                extraArgs = ["-f"];
+                };
+            };
 
-                                subvolumes = {
-                                    "/root" = {
-                                        mountpoint = "/";
-                                    };
-                                    "/persist" = {
-                                        mountOptions = ["subvol=persist" "noatime"];
-                                        mountpoint = "/persist";
-                                    };
-                                    "/nix" = {
-                                        mountOptions = ["subvol=nix" "noatime"];
-                                        mountpoint = "/nix";
-                                    };
-                                    "/swap" = {
-                                        mountpoint = "/.swapvol";
-                                        swap.swapfile.size = "4G";
-                                    };
-                                };
-                            };
+            lvm_vg.main.lvs = {
+                nix.size = "100G";
+                persist.size = "100G";
+                swap = {
+                    size = "30G";
+                    lvm_type = "thinlv";
+                    pool = "thin-main";
+                    content = {
+                        type = "swap";
+                        randomEncryption = true;
+                    };
+                };
+                crypt-home = {
+                    size = "100G";
+                    lvm_type = "thinlv";
+                    pool = "thin-main";
+                    content = {
+                        type = "luks";
+                        name = "home";
+                        content = {
+                            type = "filesystem";
+                            format = "btrfs";
+                            mountOptions = [ "noatime" ];
                         };
                     };
                 };
